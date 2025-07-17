d33eba26de69159750d34818bb35eb221a4c34a5
mahathi somisetty <mahathisomisetty777@gmail.com>
2025-07-17 12:35:09 +0530

    folders and files added

---
+++ javascript/hello.js
@@ -0,0 +1,58 @@
+// //program to print even between 1 to 100
+// for(let i=1; i<=100;i++){
+//     if(i %2 ==0){
+//         console.log(i)
+//     }
+// }
+// //program to print odd between 1 to 100
+// for(let i=1; i<=100;i++){
+//     if(i %2 ==1){
+//         console.log(i)
+//     }
+// }
+
+// //multiples of 3 and 4 between 1 to 100
+// for(let i=1; i<=100;i++){
+//     if(i %3 ==0 || i%4==0){
+//         console.log(i)
+//     }
+// }
+//
+// for(i=1;i<=20;i++){
+    
+//         console.log(i*7) 
+
+// }
+//sum of first 100 numbers
+// let count=0
+// for(i=0;i<=100;i++)
+// {
+//   count= count + i
+
+// }
+// console.log(count)
+// //factorial of a number
+// fact=1
+// for( let i=1;i<=5;i++){
+//     fact=fact*i
+// }
+// console.log(fact)
+//pgm to find product of multiples of 12 between 500 and 1200
+let product=1
+for(let i=500; i<=1200;i++){
+     if(i %12 ==0){
+     product*=i
+     }
+ }
+ console.log(product)
+//pgm to find product of multiples of 17 between 1000 and 2000
+
+ product=1
+ for( let i=1000;i<=2000;i++){
+    if(i%17==0){
+        product*=i
+    }
+ }
+ console.log(product)
+
+ 
---
+++ javascript/switch.js
@@ -0,0 +1,6 @@
+let day=2
+switch(day){
+    case 1:{
+        console.log("monday")
+    }
+}
