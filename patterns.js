13021d8098f9db8f2eb3986ec48e4a3c2bad9da0
mahathi somisetty <mahathisomisetty777@gmail.com>
2025-08-11 23:56:30 +0530

    added files and style

--- patterns.js
+++ patterns.js
@@ -32,10 +32,30 @@
 //     }
 //     console.log()
 // }
-for(let i=1;i<5;i++){
-    for(let j=0;j<5;j++){
-        process.stdout.write(`${" "}${i*j}`)
-    }
-    console.log()
+// for(let i=1;i<5;i++){
+//     for(let j=0;j<5;j++){
+//         process.stdout.write(`${" "}${i*j}`)
+//     }
+//     console.log()
+// }
+// console.log("* ".repeat(5));
+for(let i=1;i<=5;i++){
+    console.log("* ".repeat(i));
 }
-
+console.log("-".repeat(30));  
+for(let j=5;j>=1;j--){
+    console.log("*".repeat(j));
+}
+for (let i = 1; i <= 5; i++) {
+    console.log("  ".repeat(5 - i) + "* ".repeat(i));
+}
+for (let i = 1; i <= 5; i++) {
+    console.log(" ".repeat(5 - i) + "* ".repeat(i));
+}
+for (let i = 1; i <= 5; i++) {
+    let line = "";
+    for (let j = 1; j <= i; j++) {
+        line += j + " ";
+    }
+    console.log(line);
+}
